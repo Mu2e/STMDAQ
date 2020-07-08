93e4f86160b59a2f659879e652285b5ca8a0c154
Nam Tran <thnam.me@gmail.com>
2020-07-08 12:37:48 -0500

    corrected algorithm, seems working properly now

--- tests/zero-suppression/main.c
+++ tests/zero-suppression/main.c
@@ -12,61 +12,17 @@
 void importWaveform(char * line,  uint16_t * wf);
 void printWaveform(uint16_t * wf, uint16_t len);
 void printWindow(uint16_t * wf, uint16_t len);
-int findRisingEdge(uint16_t * wf, uint16_t len, uint16_t downSampling,
-      uint16_t lag, float threshold, float influence,
-      uint16_t * output) {
-
-   uint64_t mean = 0;
-   int height = 0;
-   uint64_t variance = 0;
-
-   // calculate first mean and variance
-   for (uint16_t i = 0; i < lag * downSampling; i+= downSampling)
-      mean += wf[i];
-   mean /= lag;
-
-   for (uint16_t i = 0; i < lag * downSampling; i+= downSampling)
-      variance += (wf[i] - mean) * (wf[i] - mean);
-   variance /= lag;
-
-   int foundEdge = 0;
-   uint16_t edgeStart = 0;
-   uint16_t nSamplesInEdge = 0;
-
-   for (uint16_t i = lag * downSampling; i < len; i+= downSampling) {
-      uint64_t oldMean = mean;
-      height = wf[i] - oldMean;
-
-      if ((height * height > variance * threshold * threshold) && (height >= 0)){
-         if (foundEdge == 0) {
-            foundEdge = 1;
-            nSamplesInEdge = 1;
-         }
-         else {
-            nSamplesInEdge++;
-         }
-      }
-
-      if (nSamplesInEdge == 2) {
-         uint16_t start, stop;
-         if (i - downSampling - PRE < 0)
-            start = 0;
-         else
-            start = i - downSampling - PRE;
-
-         if (i - downSampling + POST >= len)
-            stop = len;
-         else
-            stop = i - downSampling + POST;
-
-         memcpy(output, wf + start, (stop - start) * sizeof(uint16_t));
-         return (stop - start);
-
-         /* return i - downSampling; */
-      }
-   }
-   return 0;
-}
+// find rising edge function:
+// - inputs:
+//    - waveform: unsigned 16-bit integer
+//    - length of waveform: unsigned 16-bit integer
+//    - windowLen: moving window width, should be a power of 2 to make division
+//    easier
+//    - threshold: float, multiple of standard deviation 
+// - output: unsigned 16-bit integer, zero-suppressed waveform
+// - return: length of the output
+uint16_t findRisingEdge(uint16_t * wf, uint16_t len,
+      uint16_t windowLen, float threshold, uint16_t * output);
 
 int main() {
     FILE *fp = fopen("../liverpoolPulses.txt", "r");
@@ -83,7 +39,8 @@ int main() {
     printWaveform(wf, 20);
 
     uint16_t output[WF_LENGTH];
-    int outputLen = findRisingEdge(wf, WF_LENGTH, 4, 8, 4.0, 0., output);
+    int outputLen = findRisingEdge(wf, WF_LENGTH, 128, 4.0, output);
+    printf("outputLen %d\n", outputLen);
 
     printWaveform(output, outputLen);
 
@@ -117,3 +74,54 @@ void printWindow(uint16_t *data, uint16_t len) {
     }
 }
 
+uint16_t findRisingEdge(uint16_t * wf, uint16_t len,
+      uint16_t windowLen, float threshold, uint16_t * output) {
+
+   int64_t ped = 0;
+   int64_t var = 0;
+
+   // calculate first ped and var
+   for (uint16_t i = 0; i < windowLen; i++)
+      ped += wf[i];
+   ped /= windowLen;
+
+   for (uint16_t i = 0; i < windowLen; i++)
+      var += (wf[i] - ped) * (wf[i] - ped);
+   var /= windowLen;
+   printf("ped %llu, var %llu\n", ped, var);
+
+   for (uint16_t i = windowLen; i < len; i++) {
+      int64_t oldped = ped;
+      int height = wf[i] - oldped;
+
+      // only find positive edge
+      if ((height * height > var * threshold * threshold) && (height >= 0)){
+         // check next 2 samples for positive gradient
+         if (i+2 >= len) { // just return 0 if at the end of data
+            return 0;
+         }
+         else {
+            if ((wf[i+2] > wf[i+1]) && (wf[i+1] > wf[i])) { // this is it!
+               uint16_t firstSample, lastSample;
+               if (i - PRE < 0)
+                  firstSample = 0;
+               else
+                  firstSample = i - PRE;
+
+               if (i + POST >= len)
+                  lastSample = len;
+               else
+                  lastSample = i + POST;
+
+               memcpy(output, wf + firstSample, (lastSample - firstSample) * sizeof(uint16_t));
+               return (lastSample - firstSample);
+            }
+         }
+      }
+
+      ped += (wf[i] - wf[i - windowLen]) / windowLen;
+      var += (wf[i] - wf[i - windowLen]) * (wf[i] + wf[i - windowLen] - ped - oldped) / windowLen;
+      /* printf("sample %d: y %d, ped %lld, var %lld\n", i, wf[i], ped, var); */
+   }
+   return 0;
+}
